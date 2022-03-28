fe32704e6546141c8a519b952c5e8304a32c74c9
Maggie Chen <maggieyudanchen@gmail.com>
2022-03-27 21:54:45 -0700

    Fix orientation of images

--- InteriorMapping/Main.cpp
+++ InteriorMapping/Main.cpp
@@ -42,7 +42,7 @@ int Main::Run()
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
-	//stbi_set_flip_vertically_on_load(true);
+	stbi_set_flip_vertically_on_load(true);
 
 	static std::vector<std::string> textureImages = {
 		"0.png",
