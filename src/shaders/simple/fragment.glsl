ce7d12b98f6091ff86479fb9f864fb0372810108
Maggie Chen <maggieyudanchen@gmail.com>
2022-03-30 23:51:46 -0700

    Fix bottom y plane

--- src/shaders/simple/fragment.glsl
+++ src/shaders/simple/fragment.glsl
@@ -27,9 +27,9 @@ void main()
 	vec3 index = parametricT * eye + vec3(TexCoord.xy, 1.0);
 	
 	// Rotation
+	// rotate the final position based on the direction the normal faces
+	// so that the walls don't jump when we turn a corner
 	{
-		// rotate the final position based on the direction the normal faces
-		// so that the walls don't jump when we turn a corner
 		vec3 rotatedX = cross(Tangent, Normal);
 		mat3 rot = mat3(rotatedX, Tangent, Normal);
 
@@ -37,11 +37,11 @@ void main()
 	}
 
 	FragColor = texture(cubemap, index);
+
+	// DEBUGGING
 //	FragColor = vec4(eye / 2 + vec3(0.5, 0.5, 0.5), 1.0); // OK
 //	FragColor = vec4(parametricT, 0.0, 0.0, 1.0); // PROBLEM here. 
-//
-//	float x = abs(1/eye.x);
-//	FragColor = vec4(x / 10, 0.0, 0.0, 1.0); // PROBLEM here. 
-
+//	float y = abs(TexCoord.y/eye.y);
+//	FragColor = vec4(y / 10, 0.0, 0.0, 1.0); // PROBLEM here. 
 //	FragColor = vec4(vec3(TexCoord.xy, 1.0), 1.0); // OK
 }
