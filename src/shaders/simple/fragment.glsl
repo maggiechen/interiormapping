dbc37bf2c9b90b90aa456b10684cfdba8ab43fb8
Maggie Chen <maggieyudanchen@gmail.com>
2022-04-02 15:49:11 -0700

    Cleanup of fragment shader

--- src/shaders/simple/fragment.glsl
+++ src/shaders/simple/fragment.glsl
@@ -14,23 +14,34 @@ uniform float RoomHeight;
 // We assume the pivot of the object is in the very center of the mesh.
 // We assume the object's bottom is at y = 0
 
-vec3 interiorSpaceToObjectSpace(float spaceVal)
+
+// DEBUG FUNCTIONS ============
+//
+//vec3 interiorSpaceToObjectSpace(float spaceVal)
+//{
+//	float piece = spaceVal * 0.5 + 0.5;
+//	return vec3(piece, piece, piece);
+//}
+//
+
+//float sigmoid(float f)
+//{
+//	float e = 2.71828;
+//	float exponent = pow(e, -f);
+//	return 1 / (1 + exponent);
+//}
+
+
+float interiorSpaceToObjectSpace1(float spaceVal)
 {
-	float piece = spaceVal * 0.5 + 0.5;
-	return vec3(piece, piece, piece);
+	return spaceVal * 0.5 + 0.5;
 }
 
-float objectSpaceToInteriorSpace(float f)
+float objectSpaceToInteriorSpace1(float f)
 {
 	return 2 * f - 1;
 }
 
-float sigmoid(float f)
-{
-	float e = 2.71828;
-	float exponent = pow(e, -f);
-	return 1 / (1 + exponent);
-}
 
 void main()
 {
@@ -48,34 +59,35 @@ void main()
 	float roomCountContinuous = Position.y/RoomHeight;
 
 	// on [-1, 1]
-	float yTop = objectSpaceToInteriorSpace(ceil(roomCountContinuous) * RoomHeightInObjectSpace);
-	float yBottom = objectSpaceToInteriorSpace(floor(roomCountContinuous) * RoomHeightInObjectSpace);
+	float yTop = objectSpaceToInteriorSpace1(ceil(roomCountContinuous) * RoomHeightInObjectSpace);
+	float yBottom = objectSpaceToInteriorSpace1(floor(roomCountContinuous) * RoomHeightInObjectSpace);
 	
+	// Get parameter T for optimal y plane intersection
 	float yTopIntersectT = yTop/eye.y-TexCoord.y/eye.y;
-
 	float yBottomIntersectT = yBottom/eye.y-TexCoord.y/eye.y;
 	float yIntersectionT = max(yTopIntersectT, yBottomIntersectT);
 
+	// Get parameter T for optimal x plane intersection
 	float xBottomIntersectT = (1/eye.x) - (TexCoord.x/eye.x);
 	float xTopIntersectT = (-1/eye.x) - (TexCoord.x/eye.x);
-
 	float xIntersectionT = max(xBottomIntersectT, xTopIntersectT);
-//	float xIntersectionT = abs(1/eye.x) - (TexCoord.x/eye.x);
 
-
+	// Get parameter T for optimal intersection among x and y plane intersections
 	float xyIntersectionT = min(xIntersectionT, yIntersectionT);
+
+	// Get parameter T for optimal intersection among x, y, and z plane intersections
 	float parametricT = min(
 		xyIntersectionT,
 		-2/eye.z); // assume the z component of the eye vector is in the positive z axis
 
-	// need z=1.0 because the raycast starts from the entry position, which is not the origin
+	// need z = 1.0 because the raycast starts from the entry position, which is not the origin
 	// of the interior space, but 1 unit before it.
 	vec3 intersectPoint = parametricT * eye + vec3(TexCoord.xy, 1.0);
 
+	// Scale texture indexing so that wall samples stretch and repeat to fit room size
 	vec3 textureCoord = vec3(
 		intersectPoint.x,
-		intersectPoint.y,
-//		2 * (intersectPoint.y - yBottom)/RoomHeightInObjectSpace - 1,
+		2 * (interiorSpaceToObjectSpace1(intersectPoint.y) - interiorSpaceToObjectSpace1(yBottom))/RoomHeightInObjectSpace - 1,
 		intersectPoint.z
 	);
 	
@@ -86,7 +98,7 @@ void main()
 		vec3 rotatedX = cross(Tangent, Normal);
 		mat3 rot = mat3(rotatedX, Tangent, Normal);
 
-		textureCoord = rot * intersectPoint;
+		textureCoord = rot * textureCoord;
 	}
 
 	FragColor = texture(CubeMap, textureCoord);
