d76d21cfe0d5f96e32aec6a647ea0e3d24650c59
Maggie Chen <maggieyudanchen@gmail.com>
2022-04-02 15:35:11 -0700

    Fix side walls

--- src/shaders/simple/fragment.glsl
+++ src/shaders/simple/fragment.glsl
@@ -14,6 +14,24 @@ uniform float RoomHeight;
 // We assume the pivot of the object is in the very center of the mesh.
 // We assume the object's bottom is at y = 0
 
+vec3 interiorSpaceToObjectSpace(float spaceVal)
+{
+	float piece = spaceVal * 0.5 + 0.5;
+	return vec3(piece, piece, piece);
+}
+
+float objectSpaceToInteriorSpace(float f)
+{
+	return 2 * f - 1;
+}
+
+float sigmoid(float f)
+{
+	float e = 2.71828;
+	float exponent = pow(e, -f);
+	return 1 / (1 + exponent);
+}
+
 void main()
 {
     vec3 bitangent = cross(Tangent, Normal);
@@ -24,35 +42,53 @@ void main()
 	// the vector going towards the interior surface from the entry position
     vec3 eye = -normalize(worldToTangent * (EyePos - Position));
 	
+	// on [0, 1]
 	float RoomHeightInObjectSpace = RoomHeight * ObjectPos.y / Position.y;
-	float yTop = ceil(Position.y/RoomHeight) * RoomHeightInObjectSpace;
-	float yBottom = floor(Position.y/RoomHeight) * RoomHeightInObjectSpace;
-	
-	float yIntersectionT = min(
-		yTop/eye.y-TexCoord.y/eye.y,
-		yBottom/eye.y-TexCoord.y/eye.y);
 
-	float parametricT = min(min(
-		abs(1/eye.x) - TexCoord.x/eye.x,
-		yIntersectionT),
+	float roomCountContinuous = Position.y/RoomHeight;
+
+	// on [-1, 1]
+	float yTop = objectSpaceToInteriorSpace(ceil(roomCountContinuous) * RoomHeightInObjectSpace);
+	float yBottom = objectSpaceToInteriorSpace(floor(roomCountContinuous) * RoomHeightInObjectSpace);
+	
+	float yTopIntersectT = yTop/eye.y-TexCoord.y/eye.y;
+
+	float yBottomIntersectT = yBottom/eye.y-TexCoord.y/eye.y;
+	float yIntersectionT = max(yTopIntersectT, yBottomIntersectT);
+
+	float xBottomIntersectT = (1/eye.x) - (TexCoord.x/eye.x);
+	float xTopIntersectT = (-1/eye.x) - (TexCoord.x/eye.x);
+
+	float xIntersectionT = max(xBottomIntersectT, xTopIntersectT);
+//	float xIntersectionT = abs(1/eye.x) - (TexCoord.x/eye.x);
+
+
+	float xyIntersectionT = min(xIntersectionT, yIntersectionT);
+	float parametricT = min(
+		xyIntersectionT,
 		-2/eye.z); // assume the z component of the eye vector is in the positive z axis
 
 	// need z=1.0 because the raycast starts from the entry position, which is not the origin
 	// of the interior space, but 1 unit before it.
 	vec3 intersectPoint = parametricT * eye + vec3(TexCoord.xy, 1.0);
 
-	vec3 textureCoord = vec3(intersectPoint.x, (intersectPoint.y - yBottom)/RoomHeightInObjectSpace, intersectPoint.z);
+	vec3 textureCoord = vec3(
+		intersectPoint.x,
+		intersectPoint.y,
+//		2 * (intersectPoint.y - yBottom)/RoomHeightInObjectSpace - 1,
+		intersectPoint.z
+	);
 	
 	// Rotation
 	// rotate the final position based on the direction the normal faces
 	// so that the walls don't jump when we turn a corner
-//	{
-//		vec3 rotatedX = cross(Tangent, Normal);
-//		mat3 rot = mat3(rotatedX, Tangent, Normal);
-//
-//		textureCoord = rot * textureCoord;
-//	}
-//
+	{
+		vec3 rotatedX = cross(Tangent, Normal);
+		mat3 rot = mat3(rotatedX, Tangent, Normal);
+
+		textureCoord = rot * intersectPoint;
+	}
+
 	FragColor = texture(CubeMap, textureCoord);
 
 	// DEBUGGING
@@ -62,5 +98,22 @@ void main()
 //	float y = abs(TexCoord.y/eye.y);
 //	FragColor = vec4(y / 10, 0.0, 0.0, 1.0); // PROBLEM here. 
 //	FragColor = vec4(vec3(TexCoord.xy, 1.0), 1.0); // OK
-	FragColor = vec4(textureCoord * 0.5 + vec3(0.5, 05, 0.5), 1.0);
+//	FragColor = vec4(textureCoord * 0.5 + vec3(0.5, 05, 0.5), 1.0);
+//	FragColor = vec4(Position.y, Position.y, Position.y, 1.0);
+//	FragColor = vec4(roomCountContinuous, roomCountContinuous, roomCountContinuous, 1.0);
+
+//	FragColor = vec4(
+//		ceil(roomCountContinuous) * RoomHeightInObjectSpace,
+//		ceil(roomCountContinuous) * RoomHeightInObjectSpace,
+//		ceil(roomCountContinuous) * RoomHeightInObjectSpace, 1.0);
+//	FragColor = vec4(interiorSpaceToObjectSpace(yIntersectionT * eye.y + TexCoord.y), 1.0);
+	
+//	FragColor = vec4(vec3(yTopIntersectT, yTopIntersectT, yTopIntersectT), 1.0);
+//	FragColor = vec4(vec3(yBottomIntersectT, yBottomIntersectT, yBottomIntersectT), 1.0);
+//	FragColor = vec4(vec3(parametricT, parametricT, parametricT), 1.0);
+//	FragColor = vec4(intersectPoint, 1.0);
+
+//	FragColor = vec4(sigmoid(10* eye.x), 0.0, 0.0, 1.0);
+//	FragColor = vec4(vec3(xyIntersectionT, xyIntersectionT, xyIntersectionT), 1.0);
+//	FragColor = vec4(interiorSpaceToObjectSpace(yBottom), 1.0);
 }
